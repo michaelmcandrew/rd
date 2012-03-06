93ba7bcf6a2dd4c4c171f25eb1f9fce57473cf21
KatyMoon <katyjockelson@thirdsectordesign.org>
2012-03-06 18:21:40 +0000

    custom theme

---
+++ drupal/sites/all/themes/restless/template.php
@@ -0,0 +1,9 @@
+<?php
+// Add some cool text to the search block form
+function restless_form_alter(&$form, &$form_state, $form_id) {
+  if ($form_id == 'search_block_form') {
+    // HTML5 placeholder attribute
+    $form['search_block_form']['#attributes']['placeholder'] = t('Search');
+  }
+}
+?>
